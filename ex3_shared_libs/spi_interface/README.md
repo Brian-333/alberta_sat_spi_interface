123808cba4167e66a2d99b16a5b4e61563cf45f0
Brian <shihhsu1@ualberta.ca>
2024-08-29 23:22:03 -0600

    temp: power on and power off seq for OLED

--- ex3_shared_libs/spi_interface/README.md
+++ ex3_shared_libs/spi_interface/README.md
@@ -1,12 +1,25 @@
 # Interfaces for connecting components
 
-This library provides interfaces which are use by handlers and allow them to communicate with external peripherals, such as subsystems and payloads.
+This library provides interfaces which are use by handlers and allow them to communicate with external peripherals, such as subsystems and payloads. OLED power on and off sequences are included
 
 ## What is this
 
 Read and send functions are part of the SpiInterface struct and can be called whenever a process wants to simulate communicating with a peripheral.
 
 ## Testing
+
+### OLED SPI sub
+- [Pmod OLED](https://digilent.com/reference/pmod/pmodoled/reference-manual)
+    - Pmod JE pins
+        - D/C: 1
+        - Reset: 2
+        - VBATC: 3
+        - VDDC: 4
+    - Corresponding CHIP[GPIO](https://crates.io/crates/gpio-cdev)1
+        - D/C: 0
+        - Reset: 1
+        - VBATC: 2
+        - VDDC: 3
 
 ### Testing the SpiInterface
 
--- ex3_shared_libs/spi_interface/src/lib.rs
+++ ex3_shared_libs/spi_interface/src/lib.rs
@@ -9,6 +9,7 @@ extern crate spidev;
 use std::io::{Error, Read, Write};
 use spidev::{Spidev, SpidevOptions, SpiModeFlags};
 use tcp_interface::Interface;
+use gpio_cdev::{Chip, LineRequestFlags};
 
 pub const BUFFER_SIZE: usize = 1024;
 
@@ -22,11 +23,59 @@ impl SpiInterface {
         let mut spi = Spidev::open(path)?;
         let options = SpidevOptions::new()
             .bits_per_word(8)
-            .max_speed_hz(20_000)
+            .max_speed_hz(500_000)
             .mode(SpiModeFlags::SPI_MODE_0)
             .build();
         spi.configure(&options)?;
         Ok(SpiInterface { spi })
+    }
+
+    pub fn oled_power_on_seq(gpio_path: &str) -> Result<(), gpio_cdev::Error> {
+        let mut chip = Chip::new(gpio_path)?; // "/dev/gpiochip1" for default chip
+        let dc_line = chip.get_line(0)?;
+        let rst_line = chip.get_line(1)?;
+        let vbatc_line = chip.get_line(2)?;
+        let vddc_line = chip.get_line(3)?;
+
+        let dc_handler = dc_line.request(LineRequestFlags::OUTPUT, 0, "dc")?;
+        let rst_handler = rst_line.request(LineRequestFlags::OUTPUT, 0, "rst")?;
+        let vbatc_handler = vbatc_line.request(LineRequestFlags::OUTPUT, 0, "vbatc")?;
+        let vddc_handler = vddc_line.request(LineRequestFlags::OUTPUT, 0, "vddc")?;
+
+        // Power on sequence
+        vddc_handler.set_value(1)?;
+        // TODO
+        // Apply power to VDD.
+        // Send Display Off command.
+        // Initialize display to desired operating mode.
+        // Clear screen.
+        // Apply power to VBAT.
+        // Delay 100ms.
+        // Send Display On command.
+
+        Ok(())
+    }
+
+    pub fn oled_power_off_seq(gpio_path: &str) -> Result<(), gpio_cdev::Error> {
+        let mut chip = Chip::new(gpio_path)?; // "/dev/gpiochip1" for default chip
+        let dc_line = chip.get_line(0)?;
+        let rst_line = chip.get_line(1)?;
+        let vbatc_line = chip.get_line(2)?;
+        let vddc_line = chip.get_line(3)?;
+
+        let dc_handler = dc_line.request(LineRequestFlags::OUTPUT, 0, "dc")?;
+        let rst_handler = rst_line.request(LineRequestFlags::OUTPUT, 0, "rst")?;
+        let vbatc_handler = vbatc_line.request(LineRequestFlags::OUTPUT, 0, "vbatc")?;
+        let vddc_handler = vddc_line.request(LineRequestFlags::OUTPUT, 0, "vddc")?;
+
+        // Power off sequence
+        // TODO
+        // Send Display Off command
+        vbatc_handler.set_value(0)?;
+        // Delay 100ms
+        vddc_handler.set_value(0)?;
+
+        Ok(())
     }
 }
 
