dbaeee5edac33238254884bfe245ed42062c91e7
Brian Lin <shihhsu1@ualberta.ca>
2024-09-28 16:09:00 -0600

    backtrace

--- ex3_shared_libs/pmod_oled/src/main.rs
+++ ex3_shared_libs/pmod_oled/src/main.rs
@@ -1,6 +1,8 @@
 use pmod_oled::PmodOled;
+use std::env;
 
 fn main() {
     let mut pmod_oled = PmodOled::new(1, 2, 3, 4, 5, 6);
+    env::set_var("RUST_BACKTRACE", "1");
     pmod_oled.turn_on("/dev/spidev1.0").unwrap();
 }
