f03fc70b3a9e219cef2be5bc216ccba902b35d0a
Brian Lin <shihhsu1@ualberta.ca>
2024-09-28 16:11:06 -0600

    update path

--- ex3_shared_libs/pmod_oled/src/main.rs
+++ ex3_shared_libs/pmod_oled/src/main.rs
@@ -4,5 +4,5 @@ use std::env;
 fn main() {
     let mut pmod_oled = PmodOled::new(1, 2, 3, 4, 5, 6);
     env::set_var("RUST_BACKTRACE", "1");
-    pmod_oled.turn_on("/dev/spidev1.0").unwrap();
+    pmod_oled.turn_on("/dev/gpiochip0").unwrap();
 }
